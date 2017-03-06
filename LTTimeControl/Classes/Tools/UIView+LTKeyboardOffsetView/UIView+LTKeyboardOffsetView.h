351b077f3158cad2b598b03f32b08b2913110e44
孟令通 <ak-7755@qq.com>
2017-03-07 00:41:44 +0800

    自定义键盘补偿视图 1. 通过runtime增加代理 2. 通过runtime增加键盘与第一响应者缝隙属性 3. 设置监听中心，监听键盘响应事件

--- LTTimeControl/Classes/Tools/UIView+LTKeyboardOffsetView/UIView+LTKeyboardOffsetView.h
+++ LTTimeControl/Classes/Tools/UIView+LTKeyboardOffsetView/UIView+LTKeyboardOffsetView.h
@@ -8,6 +8,34 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol LTKeyboardOffsetViewDelegate <NSObject>
+
+@required
+/**
+ *  弹出键盘时，自定义视图向上移动的高度
+ *
+ *  @param firstResponder 第一响应者
+ *  @param keyboardHeight 当前弹出键盘的高度
+ *  @param offsetHeight   默认偏移高度
+ *
+ *  @return 视图向上移动的高度
+ */
+- (CGFloat)offsetHeightWithFirstResponder:(UIView *)firstResponder
+                           keyboardHeight:(CGFloat)keyboardHeight
+                             offsetHeight:(CGFloat)offsetHeight;
+
+@end
+
+/* 键盘补偿工具，避免键盘遮挡输入框 */
 @interface UIView (LTKeyboardOffsetView)
+
+/** 键盘与第一响应者的缝隙，默认6.0 */
+@property (nonatomic, assign) CGFloat keyboardGap;
+
+@property (nonatomic, weak) id<LTKeyboardOffsetViewDelegate> ltKeyboardOffsetViewDelegate;
+
+- (void)openKeyboardOffsetView;
+
+- (void)closeKeyboardOffsetView;
 
 @end
--- LTTimeControl/Classes/Tools/UIView+LTKeyboardOffsetView/UIView+LTKeyboardOffsetView.m
+++ LTTimeControl/Classes/Tools/UIView+LTKeyboardOffsetView/UIView+LTKeyboardOffsetView.m
@@ -8,6 +8,75 @@
 
 #import "UIView+LTKeyboardOffsetView.h"
 
+#import <objc/runtime.h>
+
 @implementation UIView (LTKeyboardOffsetView)
+
+static char kkeyboardGap;
+
+static char kltKeyboardOffsetViewDelegate;
+
+
+// 由于类目不可以直接添加属性，我们可以通过 runtime 的 objc_setAssociatedObject 为 UIView 动态添加属性，和类进行关联
+- (void)setKeyboardGap:(CGFloat)keyboardGap
+{
+    objc_setAssociatedObject(self, &kkeyboardGap, [NSNumber numberWithFloat:keyboardGap], OBJC_ASSOCIATION_RETAIN);
+}
+
+- (CGFloat)keyboardGap
+{
+    if (objc_getAssociatedObject(self, &kkeyboardGap) == nil) {
+        return 6.0;
+    }
+    
+    return [objc_getAssociatedObject(self, &kkeyboardGap) floatValue];
+}
+
+- (void)setLtKeyboardOffsetViewDelegate:(id<LTKeyboardOffsetViewDelegate>)ltKeyboardOffsetViewDelegate
+{
+    objc_setAssociatedObject(self, &kltKeyboardOffsetViewDelegate, ltKeyboardOffsetViewDelegate, OBJC_ASSOCIATION_RETAIN);
+}
+
+- (id<LTKeyboardOffsetViewDelegate>)ltKeyboardOffsetViewDelegate
+{
+    return objc_getAssociatedObject(self, &kltKeyboardOffsetViewDelegate);
+}
+
+// 打开键盘补偿视图
+- (void)openKeyboardOffsetView
+{
+    
+    //对系统的某些事件时作出响应只要注册一个观察者即可
+    //事件1：每次键盘显示后得到通知就得关心 UIKeyboardDidShowNotification 事件:这里的post是系统自己定义好的，不需要写，只需要添加自己是观察者，且做出相应就可以，观察者名字是系统的，记得不观察的时候要移除监听观察者事件
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillAppear:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillDisappear:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+    
+    
+}
+
+// 关闭键盘补偿视图
+- (void)closeKeyboardOffsetView
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)keyboardWillAppear:(NSNotification *)notification
+{
+    
+}
+
+- (void)keyboardWillDisappear:(NSNotification *)notification
+{
+    
+}
 
 @end
