b92411efd326f1074fcc364e08715f5be24f7490
孟令通 <ak-7755@qq.com>
2017-03-06 10:24:46 +0800

    增加键盘补充视图的类目文件

---
+++ LTTimeControl/Classes/Tools/UIView+LTKeyboardOffsetView/UIView+LTKeyboardOffsetView.h
@@ -0,0 +1,13 @@
+//
+//  UIView+LTKeyboardOffsetView.h
+//  LTTimeControl
+//
+//  Created by 孟令通 on 17/3/6.
+//  Copyright © 2017年 LryMlt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (LTKeyboardOffsetView)
+
+@end
